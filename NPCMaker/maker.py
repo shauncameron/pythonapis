65dfc4002fb037383c1546688c08f29e55c007c0
shauncameron <70017478+shauncameron@users.noreply.github.com>
2021-06-24 20:51:23 +0100

    Create maker.py

---
+++ NPCMaker/maker.py
@@ -0,0 +1,98 @@
+nm1 = ["Abarat","Adamar","Adorellan","Adresin","Aelrindel","Aerendyl","Aeson","Afamrail","Agandaur","Agis","Aias","Aiduin","Aien","Ailas","Ailduin","Ailen","Ailluin","Ailmar","Ailmer","Ailmon","Ailre","Ailred","Ailuin","Ailwin","Aimar","Aimer","Aimon","Airdan","Aire","Aired","Aithlin","Aiwin","Akkar","Alabyran","Alaion","Alas","Alen","Alinar","Alluin","Almar","Almer","Almon","Alok","Alosrin","Alre","Alred","Althidon","Alwin","Amrynn","Andrathath","Anfalen","Anlyth","Aolis","Aquilan","Arathorn","Arbane","Arbelladon","Ardreth","Ardryll","Arel","Arlen","Arun","Ascal","Athtar","Aubron","Aumanas","Aumrauth","Avourel","Ayas","Ayduin","Ayen","Ayluin","Aymar","Aymer","Aymon","Ayre","Ayred","Aywin","Belanor","Beldroth","Bellas","Beluar","Biafyndar","Bialaer","Braern","Cailu","Camus","Castien","Chathanglas","Cohnal","Conall","Connak","Cornaith","Corym","Cyran","Dain","Dakath","Dalyor","Darcassan","Darfin","Darthoridan","Darunia","Deldrach","Delmuth","Delsaran","Devdan","Drannor","Druindar","Durlan","Durothil","Dyffros","Edwyrd","Edyrm","Ehlark","Ehrendil","Eilauver","Elaith","Elandorr","Elas","Elashor","Elauthin","Eldaerenth","Eldar","Eldrin","Elduin","Elen","Elephon","Elidyr","Elion","Elkhazel","Ellisar","Elluin","Elmar","Elmer","Elmon","Elnaril","Elorshin","Elpharae","Elre","Elred","Eltaor","Elwin","Elyon","Emmyth","Entrydal","Erendriel","Eriladar","Erlan","Erlareo","Erlathan","Eroan","Erolith","Estelar","Ethlando","Ettrian","Evindal","Faelar","Faelyn","Faeranduil","Falael","Felaern","Fenian","Feno","Feyrith","Fhaornik","Filarion","Filvendor","Filverel","Flardryn","Flinar","Folas","Folduin","Folen","Folluin","Folmar","Folmer","Folmon","Folre","Folred","Folwin","Fylson","Gaeleath","Gaelin","Galaeron","Galan","Galather","Ganamede","Gantar","Garrik","Garynnon","Giullis","Glanduil","Glarald","Glorandal","Goras","Gorduin","Goren","Gorluin","Gormar","Gormer","Gormon","Gorre","Gorred","Gorwin","Grathgor","Haemir","Hagas","Hagduin","Hagen","Hagluin","Hagmar","Hagmer","Hagre","Hagred","Hagwin","Haladavar","Halafarin","Halamar","Haldir","Halflar","Halueth","Halueve","Hamon","Haryk","Hastios","Hatharal","Horith","Hubys","Iefyr","Ievis","Ilbryen","Ilimitar","Iliphar","Illianaro","Illithor","Illitran","Ilphas","Ilrune","Ilthuryn","Ilvisar","Inchel","Inialos","Intevar","Iolas","Iolrath","Itham","Ivaran","Ivasaar","Iymbryl","Iyrandrar","Jandar","Jannalor","Jaonos","Jassin","Jhaan","Jhaartael","Jhaeros","Jonik","Jorildyn","Kailu","Katar","Katyr","Kellam","Kelvhan","Kendel","Kerym","Keryth","Kesefeon","Kharis","Khatar","Khidell","Khiiral","Khilseith","Khuumal","Khyrmin","Kieran","Kiirion","Kindroth","Kivessin","Klaern","Kolvar","Kuskyn","Kymil","Kyrenic","Kyrtaar","Laeroth","Lafarallin","Laiex","Lamruil","Larongar","Larrel","Lathai","Lathlaeril","Lhoris","Lianthorn","Llarm","Llewel","Lorsan","Luirlan","Luthais","Luvon","Lyari","Lyklor","Lysanthir","Maeral","Maiele","Malgath","Malon","Maradeim","Marikoth","Marlevaur","Melandrach","Merellien","Merith","Methild","Mhaenal","Mitalar","Mihangyl","Miirphys","Mirthal","Mlartlar","Mnementh","Morthil","Myrdin","Myriil","Myrin","Mythanar","Naertho","Naeryndam","Naesala","Narbeth","Nardual","Nasir","Navarre","Nelaeryn","Neldor","Neremyn","Nesterin","Nevarth","Nhamashal","Nieven","Nindrol","Ninleyn","Ninthalor","Niossae","Nuvian","Nylian","Nym","Nyvorlas","Olaurae","Onas","Oncith","Onvyr","Orist","Ornthalas","Orrian","Orym","Otaehryn","Othorion","Paeral","Paeris","Pelleas","Phaendar","Pharom","Phraan","Pirphal","Purtham","Pyrravyn","Pywaln","Qildor","Raeran","Raibyn","Ralnor","Ranaeril","Rathal","Reluraun","Reluvethel","Rennyn","Reptar","Respen","Revalor","Rhalyf","Rhangyl","Rhistel","Rhothomir","Rhys","Rilitar","Riluaneth","Rolim","Rothilion","Ruehnar","Ruith","Ruvaen","Ruven","Ruvyn","Rychell","Rydel","Ryfon","Ryo","Ryul","Saelethil","Saevel","Saleh","Samblar","Sanev","Scalanis","Selanar","Sharian","Shaundyl","Shyrrik","Sihnion","Silvyr","Simimar","Sinaht","Siveril","Sontar","Sudryal","Sundamar","Sylvar","Sythaeryn","Taegen","Taenaran","Taeral","Taerentym","Taleasin","Tamnaeth","Tanithil","Tannatar","Tannivh","Tannyll","Tanyl","Tanyth","Taranath","Tarathiel","Taredd","Tarron","Tasar","Tassarion","Tathaln","Thalanil","Thallan","Theodas","Theodemar","Theoden","Theodluin","Theodmer","Theodmon","Theodre","Theodred","Thuridan","Tiarsus","Tolith","Tordynnar","Toross","Traeliorn","Travaran","Triandal","Ualiar","Uevareth","Uldreyin","Urdusin","Usunaar","Uthorim","Vaalyun","Vaeril","Vamir","Varitan","Velethuil","Venali","Vesryn","Vesstan","Virion","Volodar","Voron","Vuduin","Vulas","Vulen","Vulluin","Vulmar","Vulmer","Vulmon","Vulre","Vulred","Vulwin","Wirenth","Wistari","Wyn","Wyninn","Wyrran","Yalathanil","Yesanith","Yhendorn","Ylyndar","Zaos","Zelphar","Zeno","Zhoron"];
+nm2 = ["Aelrie","Aelua","Aelynthi","Aenwyn","Aerilaya","Aerith","Ahrendue","Ahshala","Aila","Alagossa","Alais","Alanis","Alasse","Alavara","Alea","Aleesia","Alenia","Aleratha","Allannia","Allisa","Alloralla","Allynna","Almedha","Almithara","Alvaerelle","Alyndra","Amara","Amaranthae","Amarille","Amedee","Ameria","Amisra","Amnestria","Amra","Anarzee","Aneirin","Anhaern","Annallee","Ara","Arasne","Aravae","Arcaena","Ariawyn","Arilemna","Arlayna","Arnarra","Arryn","Arthion","Artin","Ashera","Ashryn","Aurae","Ava","Axilya","Ayda","Ayla","Azariah","Baerinda","Bellaluna","Bemere","Bonaluria","Burolia","Caeda","Caerthynna","Calarel","Celaena","Cellica","Chaenath","Chalia","Chalsarda","Chamylla","Chandrelle","Chasianna","Ciliren","Ciradyl","Cithrel","Clanire","Cremia","Daethie","Daratrine","Darshee","Darunia","Dasyra","Delimira","Delsanra","Dessielle","Deulara","Dilya","Dirue","Ealirel","Ecaeris","Edea","Edraele","Eirina","Elanalue","Elanil","Elasha","Elenaril","Eletha","Elincia","Elisen","Eliyen","Ellarian","Elmyra","Eloimaya","Elora","Elyon","Ena","Enania","Eshenesra","Esiyae","Essaerae","Esta","Falenas","Faraine","Farryn","Faunalyn","Fayeth","Faylen","Fhaertala","Filaurel","Filauria","Fildarae","Finnea","Gaelira","Gaerradh","Gaylia","Geminara","Ghilanna","Glynnii","Gweyir","Gwynnestri","Gylledhia","Haciathra","Haera","Halaema","Halanaestra","Hamalitia","Haramara","Helartha","Holone","Huethea","Hycis","Ialantha","Ikeshia","Ildilyntra","Ilmadia","Ilsevel","Ilyana","Ilyrana","Ilythyrra","Imizael","Immianthe","Imra","Imryll","Ioelena","Irhaal","Isarrel","Isilynor","Ithronel","Itireae","Itylara","Jastira","Jeardra","Jhaerithe","Jhanandra","Jhilsara","Kali","Kasula","Kavrala","Kaylessa","Kaylin","Keenor","Keerla","Keishara","Kenia","Kethryllia","Keya","Kilyn","Kylantha","Kythaela","Laamtora","Laerdya","Lazziar","Leena","Leilatha","Lenna","Lensa","Lethhonel","Lierin","Liluth","Lithoniel","Lixiss","Llamiryl","Llorva","Loreleia","Lura","Lusha","Lusserina","Lyeecia","Lyeneru","Lymseia","Lyndis","Lyra","Lyrei","Lythienne","Madris","Maelyrra","Maeralya","Maescia","Makaela","Malonne","Malruthiia","Mariona","Mathienne","Maylin","Meira","Melarue","Meorise","Merethyl","Merialeth","Meriel","Merlara","Mhoryga","Micaiah","Minuvae","Muelara","Myantha","Mylaela","Mylaerla","Myriani","Myrrh","Nabeora","Naesala","Naevys","Naexi","Nakiasha","Nalaea","Nambra","Namys","Nanthaliene","Neia","Nephinae","Nimeroni","Nimue","Nithenoel","Nithroel","Nuala","Nueleth","Nuovis","Nushala","Nyana","Nylathria","Ochilysse","Omylia","Osonia","Penelo","Phaerille","Phelorna","Phinara","Phyrra","Pyria","Qamara","Radelia","Raenisa","Rallientha","Rania","Ratha","Rathiain","Renestrae","Renna","Rina","Riniya","Rophalin","Rosanhi","Rosaniya","Roshia","Rubarae","Ryllae","Saelihn","Saida","Sakaala","Salihn","Sana","Saphielle","Saria","Sariandi","Sarya","Seldanna","Selphie","Selussa","Shael","Shaerra","Shalaevar","Shalana","Shalendra","Shalheira","Shalia","Shanaera","Shandalar","Shanyrria","Shelara","Shenarah","Sillavana","Sionia","Siora","Siphanien","Siraye","Solana","Soliana","Sorisana","Sumina","Syllia","Sylmare","Symania","Syndra","Syvis","Taenya","Talanashta","Talindra","Tanelia","Tanila","Tanulia","Tarasynora","Tehlarissa","Tephysea","Teriani","Thaciona","Thalia","Thaola","Thasinia","Thessalia","Tialha","Tinesi","Tiriana","Tisha","Tsarra","Tyrael","Ulesse","Umilythe","Uneathen","Urricea","Usamea","Vaeri","Valindra","Vanya","Vasati","Velatha","Verrona","Vestele","Vianola","Viessa","Wynather","Yaereene","Yalanue","Yathanae","Ygannea","Ynaselle","Yralissa","Yrathea","Yrneha","Ysildea","Yumanea","Yunaesa","Zaleria","Zentha","Zestari","Zilyana"];
+nm3 = ["Ad","Ae","Ara","Bal","Bei","Bi","Bry","Cai","Car","Chae","Cra","Da","Dae","Dor","Eil","El","Ela","En","Er","Fa","Fae","Far","Fen","Gen","Gil","Glyn","Gre","Hei","Hele","Her","Hola","Ian","Iar","Ili","Ina","Jo","Kea","Kel","Key","Kris","Leo","Lia","Lora","Lu","Mag","Mia","Mira","Mor","Nae","Neri","Nor","Ola","Olo","Oma","Ori","Pa","Per","Pet","Phi","Pres","Qi","Qin","Qui","Ralo","Rava","Rey","Ro","Sar","Sha","Syl","The","Tor","Tra","Tris","Ula","Ume","Uri","Va","Val","Ven","Vir","Waes","Wran","Wyn","Wysa","Xil","Xyr","Yel","Yes","Yin","Ylla","Zin","Zum","Zyl"];
+nm4 = ["balar","banise","bella","beros","can","caryn","ceran","cyne","dan","di","dithas","dove","faren","fiel","fina","fir","geiros","gella","golor","gwyn","hana","harice","hice","horn","jeon","jor","jyre","kalyn","kas","kian","krana","lamin","lana","lar","lee","len","leth","lynn","maer","maris","menor","moira","myar","mys","na","nala","nan","neiros","nelis","norin","peiros","petor","phine","phyra","qen","qirelle","quinal","ra","ralei","ran","rel","ren","ric","rie","rieth","ris","ro","rona","rora","roris","salor","sandoral","satra","stina","sys","thana","thyra","toris","tris","tumal","valur","varis","ven","vyre","warin","wenys","wraek","wynn","xalim","xidor","xina","xisys","yarus","ydark","ynore","yra","zana","zeiros","zorwyn","zumin"];
+nm5 = ["Adorellan","Adresin","Aelrindel","Aenwyn","Aerendyl","Aerith","Aien","Ailen","Ailre","Aimer","Aire","Aithlin","Alaion","Alais","Alanis","Alasse","Alosrin","Amra","Amrynn","Aneirin","Anfalen","Anhaern","Anlyth","Arbane","Ardreth","Arel","Ariawyn","Arryn","Arthion","Artin","Ashryn","Aubron","Avourel","Axilya","Ayen","Aymer","Ayre","Aywin","Azariah","Bellas","Bemere","Bialaer","Caeda","Calarel","Chaenath","Ciliren","Ciradyl","Cithrel","Cohnal","Conall","Cornaith","Cyran","Dain","Darunia","Ehlark","Ehrendil","Elaith","Elandorr","Elanil","Elas","Elauthin","Eldaerenth","Eldrin","Elen","Elidyr","Elion","Elisen","Ellisar","Elluin","Elnaril","Elpharae","Elred","Elyon","Emmyth","Erendriel","Eroan","Estelar","Faelyn","Falael","Falenas","Farryn","Felaern","Feno","Filaurel","Filverel","Folen","Folre","Fylson","Gaeleath","Gaelin","Gaerradh","Galan","Goras","Goren","Gweyir","Haemir","Halaema","Halamar","Haldir","Halueth","Halueve","Hamon","Horith","Hycis","Iefyr","Ilbryen","Iliphar","Ilphas","Imizael","Inchel","Irhaal","Isarrel","Isilynor","Ithronel","Ivasaar","Jandar","Jassin","Jhaan","Jorildyn","Kailu","Katar","Keenor","Kelvhan","Kendel","Keryth","Kharis","Khidell","Khiiral","Khyrmin","Kilyn","Kindreth","Kymil","Laeroth","Larrel","Lathlaeril","Lazziar","Lethonel","Lhoris","Lierin","Llewel","Lorsan","Lyari","Lysanthir","Maeral","Maiele","Malon","Malonne","Merellien","Meriel","Merith","Methild","Micaiah","Mirthal","Mnementh","Myrdin","Myriil","Myrin","Myrrh","Naesala","Naevys","Namys","Narbeth","Nasir","Navarre","Nelaeryn","Neremyn","Nesterin","Nhamashal","Nieven","Nithenoel","Nueleth","Nuovis","Nym","Orym","Paeral","Paeris","Pelleas","Phraan","Rathiain","Rennyn","Rhalyf","Riluaneth","Rolim","Ruehnar","Ruvaen","Ruven","Ruvyn","Ryllae","Ryo","Saelethil","Saelihn","Saevel","Saida","Saleh","Sanev","Selanar","Shalaevar","Shandalar","Sharian","Sinaht","Sylmare","Sylvar","Syvis","Taenaran","Taeral","Tamnaeth","Tanathil","Tannatar","Tannyll","Tanyl","Tanyth","Taranth","Tarathiel","Thalanil","Thallan","Tyrael","Uneathen","Vaeril","Vamir","Venali","Virion","Vulen","Vulmar","Vulmer","Vulwin","Wirenth","Wynather","Yesanith","Zeno"];
+
+import random 
+
+names = nm1 + nm2 + nm3 + nm4 + nm5 
+
+def makename(length):
+
+    madenames = []
+
+    for i in range(length):
+
+        name = random.choice(names)
+        name = name[0].upper() + name[1:].lower()
+        madenames.append(name)
+
+    return ' '.join(madenames)
+
+degree = ["Lawful", "Neutral", "Chaotic"]
+alignment = ["Good", "Neutral", "Evil"]
+
+def makealignment():
+
+    made = []
+
+    random.shuffle(degree)
+    random.shuffle(alignment)
+
+    made.append(degree[0])
+    made.append(alignment[0])
+
+    if made == ["Neutral", "Neutral"]:
+
+        made = ["True", "Neutral"]
+
+    return ' '.join(made) 
+
+hair_state = ["scruffy", "plaitted", "neat", "tidy", "long", "glossy", "matte", "short", "straight", "frizzy"]
+
+hair = ["dirt brown", "dark brown", "light brown", "jet black", "vanta black", "raven black", "light auburn", "dark auburn", "snow white", "shiny silver", "pale silver", "dirty blonde", "light blonde", "platinum blonde", "no"]
+
+skin = ["fair", "pale", "palish", "dark", "light", "light brown", "charred", "tanned"]
+
+depths = ["deep", "light", "dark", "hollow", "shining", "glimmering"]
+
+colours = ["gold", "crimson red", "red", "yellow", "pink", "green", "purple", "orange", "blue", "black", "brown"]
+
+def maketraits():
+
+    height = f'{random.randint(50, 68)}'
+
+    return f'{random.choice(hair_state)} {random.choice(hair)} hair,{random.choice(depths)} {random.choice(colours)} eyes,{random.choice(skin)} skin'
+
+madenames = []
+
+classes = ["cleric", "bard", "fighter", "barbarian", "monk", "warlock", "sorceror", "wizard", "druid", "ranger", "rogue", "artificer", "scholar", "butcher", "scout", "forager", "farmer", "historian", "professor", "chef", "traveler", "adventurer", "dungeoneer", "soldier", "hermit", "artist", "builder", "investigator", "guard", "Alchemist", "Baker", "Archer", "Prankster", "Fool", "Accountant"]
+
+for i, classx in enumerate(classes):
+
+    classes[i] = classx[0].upper() + classx[1:].lower()
+
+races = ["Dragonborn", "Tiefling", "Human", "Elf", "Half-Elf", "Dark-Elf", "High-Elf", "Orc", "Half-Orc", "Dwarf", "Half-Dwarf", "Halfling", "Gnome", "Half-Giant", "Half-Fey", "Half-Sprite", "Half-Sprit", "Human", "Half-Human", "Forgotten-Human", "Genasi", "Half-Dragonborn"]
+
+Politics = ["Country Guild", "Country Army", "None", "None", "Country Family", "Royal Family", "Country Family Army", "Own Family", "Self", "Friends"]
+
+
+def makepeople(amount):
+
+    madepeople = []
+
+    for i in range(amount):
+
+        madepeople.append(
+            f"""| {makename(random.randint(1, 4))} | {random.choice(["M", "F"])} | {random.choice(races)} | {random.choices(classes)[0]} | {random.choice(Politics)} | {makealignment()} | {maketraits()} |""")
+
+    return madepeople
+
+def makepages(amount):
+
+    pages = []
+
+    for i in range(amount):
+
+        totalpage = (
+    """<div class='classTable wide'>\n""" + 
+    """##### NPC Table \n""" + 
+    """| Name | Sex | Race | Class | Allegiance | Alignment | Appearance |\n""" + 
+    """|:----:|:---:|:----:|:-----:|:----------:|:---------:|:----------:|\n""" + 
+    '\n'.join(makepeople(27)) +'\n</div>'
+    )
+
+        pages.append(totalpage)
+
+    return '\n\page\n'.join(pages)
